eaf26121f9b8fb571231e1fa4bd63ddbaf6cf7e8
Nicola-Pagliara <n.pagliara1@studenti.unisa.it>
2024-06-07 17:42:56 +0200

    Version 0.1 preprocesing.

--- Support/constant.py
+++ Support/constant.py
@@ -1,7 +1,12 @@
 import os
 
 DATA_ROOT = 'Data'
+
+# Preprocessing module constants
 DoCRED_PREFIX = os.path.join(DATA_ROOT, 'DocRED')
-DATASET_TRAIN = os.path.join(DoCRED_PREFIX, 'train_annotated.json')
+DATASET_TRAIN_HUMAN = os.path.join(DoCRED_PREFIX, 'train_annotated.json')
+PREFIX_SAVE_ENT = '/entity.json'
+PREFIX_SAVE_REL = '/relations.json'
+PREFIX_SAVE_SEN = '/sentences.json'
 
 
